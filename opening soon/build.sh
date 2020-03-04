12af0b6fe90296cc30617f2539e53de35999b77e
Raju <raju@doublarddesign.com>
2020-03-04 17:57:05 +0545

    new changes

---
+++ opening soon/build.sh
@@ -0,0 +1,10 @@
+DEST="./dist"
+
+rm -fr "$DEST/"
+mkdir -p "$DEST/"
+cp -r "./css" "$DEST/"
+cp -r "./fonts" "$DEST/"
+cp -r "./images" "$DEST/"
+cp -r "./js" "$DEST/"
+cp -r "./vendor" "$DEST/"
+SCRIPT_FILENAME=index.php REQUEST_URI=/ php index.php > "./$DEST/index.html"
