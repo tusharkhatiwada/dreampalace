46cb20bc85b17cde8c650253c30410a6282f66dd
Raju Lawat <tokharaju@gmail.com>
2020-03-04 19:13:20 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ cp -r "./fonts" "$DEST/"
 cp -r "./images" "$DEST/"
 cp -r "./js" "$DEST/"
 cp -r "./vendor" "$DEST/"
+
 SCRIPT_FILENAME=index.php REQUEST_URI=/ php index.php > "./$DEST/index.html"
+
+SCRIPT_FILENAME=contact.php REQUEST_URI=/ php contact.php > "./$DEST/contact.html"
