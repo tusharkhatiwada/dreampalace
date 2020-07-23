9d420b02ab78099fa15353042719c037e67477cc
raju <raju@doublarddesign.com>
2020-07-23 10:46:36 +0545

    images added

--- index.php
+++ index.php
@@ -132,7 +132,7 @@
           </div>
         </div>
       </div>
-      <div class="site-blocks-cover overlay" style="background-image: url(images/dreampalace3.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="site-blocks-cover overlay" style="background-image: url(images/dreampalace-night-view.jpeg);" data-aos="fade" data-stellar-background-ratio="0.5">
         <div class="container">
           <div class="row align-items-center justify-content-center">
             <div class="col-md-7 text-center" data-aos="fade">
